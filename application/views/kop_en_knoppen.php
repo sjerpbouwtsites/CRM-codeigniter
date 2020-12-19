32f2c070052ca561c6d841146cbdbf6b25485dfb
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2020-12-19 03:30:13 +0100

    cleanup head of site

--- application/views/kop_en_knoppen.php
+++ application/views/kop_en_knoppen.php
@@ -1,11 +1,9 @@
 <div class='kop-en-knoppen'>
 	<article>
 		<h1>Sjerp's CRM - v3.1 - tabel: <?= $tabel_naam ?> </h1>
-		<p>Gebruik mij niet in een openbaar wifi netwerk.<br>
-			Als je het wachtwoord niet zeker weet blijf hier dan vanaf.<br>
-			<strong>Sla nooit deze wachtwoorden op je computer of browser op.</strong><br>
 
-		</p>
+		<p>Uitleg? <br>Bel sjerp: 0616541143 of<br>Mail dev@sjerpbouwtsites.nl<br>Vind je iets kut of een fout? Hier is me <a target='_blank' href='<?= base_url() ?>todo.html'>planning</a></p>
+
 	</article>
 
 	<nav id='crm'>
@@ -14,8 +12,8 @@
 		<a class='<?php echo ($tabel_naam === "contacten" ? "actief" : ""); ?>' href="<?= base_url() ?>?tabel=contacten">contacten</a>
 		<a class='<?php echo ($tabel_naam === "juridisch" ? "actief" : ""); ?>' href="<?= base_url() ?>?tabel=juridisch">juridisch</a>
 		<a class='<?php echo ($tabel_naam === "inlog" ? "actief" : ""); ?>' href="<?= base_url() ?>?tabel=inlog">inloggegevens</a>
-		<a href='mailto:info@sjerpbouwtsites.nl?subject=[CRM] - HELP!'>bij paniek</a>
-		<a target='_blank' href='<?= base_url() ?>todo.html'>planning</a>
+
+
 	</nav>
 
 	<nav id='mobiel'>
