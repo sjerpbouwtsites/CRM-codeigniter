8982d810f27f4bf38da15f0cd619d25689ac7077
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2020-12-19 03:13:27 +0100

    .

--- application/views/kop_en_knoppen.php
+++ application/views/kop_en_knoppen.php
@@ -1,21 +1,21 @@
 <div class='kop-en-knoppen'>
 	<article>
-		<h1>Sjerp's CRM - v3.0 - tabel: <?=$tabel_naam?> </h1>
+		<h1>Sjerp's CRM - v3.1 - tabel: <?= $tabel_naam ?> </h1>
 		<p>Gebruik mij niet in een openbaar wifi netwerk.<br>
-		Als je het wachtwoord niet zeker weet blijf hier dan vanaf.<br>
-		<strong>Sla nooit deze wachtwoorden op je computer of browser op.</strong><br>
+			Als je het wachtwoord niet zeker weet blijf hier dan vanaf.<br>
+			<strong>Sla nooit deze wachtwoorden op je computer of browser op.</strong><br>
 
 		</p>
 	</article>
 
 	<nav id='crm'>
-		<a class='<?php echo ($tabel_naam==="leden"?"actief":""); ?>' href="<?=base_url()?>?tabel=leden">leden</a>
-        <a class='<?php echo ($tabel_naam==="nijmegen"?"actief":""); ?>' href="<?=base_url()?>?tabel=nijmegen">nijmegen</a>
-		<a class='<?php echo ($tabel_naam==="contacten"?"actief":""); ?>' href="<?=base_url()?>?tabel=contacten">contacten</a>
-		<a class='<?php echo ($tabel_naam==="juridisch"?"actief":""); ?>' href="<?=base_url()?>?tabel=juridisch">juridisch</a>
-		<a class='<?php echo ($tabel_naam==="inlog"?"actief":""); ?>' href="<?=base_url()?>?tabel=inlog">inloggegevens</a>
+		<a class='<?php echo ($tabel_naam === "leden" ? "actief" : ""); ?>' href="<?= base_url() ?>?tabel=leden">leden</a>
+		<a class='<?php echo ($tabel_naam === "nijmegen" ? "actief" : ""); ?>' href="<?= base_url() ?>?tabel=nijmegen">nijmegen</a>
+		<a class='<?php echo ($tabel_naam === "contacten" ? "actief" : ""); ?>' href="<?= base_url() ?>?tabel=contacten">contacten</a>
+		<a class='<?php echo ($tabel_naam === "juridisch" ? "actief" : ""); ?>' href="<?= base_url() ?>?tabel=juridisch">juridisch</a>
+		<a class='<?php echo ($tabel_naam === "inlog" ? "actief" : ""); ?>' href="<?= base_url() ?>?tabel=inlog">inloggegevens</a>
 		<a href='mailto:info@sjerpbouwtsites.nl?subject=[CRM] - HELP!'>bij paniek</a>
-		<a target='_blank' href='<?=base_url()?>todo.html'>planning</a>
+		<a target='_blank' href='<?= base_url() ?>todo.html'>planning</a>
 	</nav>
 
 	<nav id='mobiel'>
