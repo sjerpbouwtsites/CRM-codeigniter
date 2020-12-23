6832d644a195edd734a015fd86f6ff67a65f1ce1
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2020-12-23 14:50:37 +0100

    db migration add

--- crm-local.session.sql
+++ crm-local.session.sql
@@ -1,2 +1,15 @@
-ALTER TABLE leden
-ADD COLUMN contact VARCHAR(255);
+/* bondgenoten
+ */
+CREATE TABLE contacten (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  naam VARCHAR(255),
+  email VARCHAR(255),
+  telefoon VARCHAR(255),
+  laatst_gezien VARCHAR(255),
+  ik_wil TEXT,
+  aantekening TEXT,
+  sector VARCHAR(255),
+  woonplaats VARCHAR(255),
+  groep VARCHAR(255),
+  contact VARCHAR(255)
+);
--- db-migration-dec-2020.sql
+++ db-migration-dec-2020.sql
@@ -18,3 +18,33 @@ CREATE TABLE CSRF (
   token VARCHAR(255),
   modified_on DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
+/* contacten, arbeiders
+ */
+CREATE TABLE contacten (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  naam VARCHAR(255),
+  email VARCHAR(255),
+  telefoon VARCHAR(255),
+  laatst_gezien VARCHAR(255),
+  ik_wil TEXT,
+  aantekening TEXT,
+  sector VARCHAR(255),
+  woonplaats VARCHAR(255),
+  groep VARCHAR(255),
+  contact VARCHAR(255);
+)
+/* bondgenoten
+ */
+CREATE TABLE bondgenoten (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  naam VARCHAR(255),
+  email VARCHAR(255),
+  telefoon VARCHAR(255),
+  laatst_gezien VARCHAR(255),
+  ik_wil TEXT,
+  aantekening TEXT,
+  sector VARCHAR(255),
+  woonplaats VARCHAR(255),
+  groep VARCHAR(255),
+  contact VARCHAR(255)
+)
