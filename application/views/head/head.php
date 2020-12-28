c63e2c79657dbf51b248d0e3d32581917d048a9c
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2020-12-28 21:45:47 +0100

    voorpagina stijling, opmaak

--- application/views/head/head.php
+++ application/views/head/head.php
@@ -14,6 +14,19 @@
 		<link rel='stylesheet' href="<?= base_url('css/style.css') ?>">
 		<meta http-equiv="Cache-control" content="no-cache">
 		<meta name="viewport" content="width=device-width">
+		<link rel="shortcut icon" href="/afb/app-icons/favicon.ico" />
+		<link rel="apple-touch-icon" sizes="57x57" href="/afb/app-icons/little-penguin-57-27563.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/afb/app-icons/little-penguin-72-27563.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/afb/app-icons/little-penguin-114-27563.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/afb/app-icons/little-penguin-120-27563.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/afb/app-icons/little-penguin-144-27563.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/afb/app-icons/little-penguin-152-27563.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/afb/app-icons/little-penguin-32-27563.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/afb/app-icons/little-penguin-16-27563.png">
+		<!--		<link rel="manifest" href="/manifest.json">-->
+		<meta name="msapplication-TileColor" content="#d73f3f">
+		<meta name="msapplication-TileImage" content="/afb/app-icons/little-penguin-144-27563.png">
+		<meta name="theme-color" content="#d73f3f">
 		<style>
 			<?= $toon_stijl ?>
 		</style>
