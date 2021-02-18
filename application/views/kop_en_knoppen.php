151b8434b02ff2b42287a35b7cdbd1f250f779b5
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-02-18 08:12:19 +0100

    entree tekst veranderd en tabelnaam toegevoegd

--- application/views/kop_en_knoppen.php
+++ application/views/kop_en_knoppen.php
@@ -7,8 +7,8 @@
 
 	<div id='sleutelaars' class='sleutelaars'>
 		<div class='sleutelaars-inner'>
-			<h2 class='versleutelde-titel versleutelde-titel--kleiner'>Sjerps end-to-end encrypted crm <small>v3.2</small></h2>
-			<h2 class='versleutelde-titel'>Ontsleutel om verder te gaan. </h2>
+			<h2 class='versleutelde-titel versleutelde-titel--kleiner'>Sjerps end-to-end encrypted crm <small>v3.2.1</small></h2>
+			<h2 class='versleutelde-titel'>Ontsleutel voor toegang<br>tot <em><?=$tabel_naam?></em> </h2>
 			<div class='sleutelaars-inner-2'>
 				<label for='ontsleutel'>
 					<input id='ontsleutel' type='password' placeholder='wachtwoord' autocapitalize="off">
