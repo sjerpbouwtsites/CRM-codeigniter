e27dcc1e8085b9faa70b149dc482b46ace4222a7
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-10-21 06:27:17 +0200

    Revert "revert1"

--- application/views/set-user.php
+++ application/views/set-user.php
@@ -5,9 +5,10 @@
 <body id='app-body'  data-base-url='<?= base_url() ?>'>
 <div class='sleutelaars'>
 <form class='sleutelaars-inner' method='POST' action='<?=base_url()?>'>
+<h2 class='versleutelde-titel versleutelde-titel--kleiner'>Sjerps end-to-end encrypted crm <small>v4</small></h2>
 <h2 class='versleutelde-titel'>Wie zijn jullie?</h2>
 <label for="ontsleutel">
-    <input name='user' id="ontsleutel" type="password" placeholder="wachtwoord" autocapitalize="off">
+    <input name='user' id="ontsleutel" placeholder="naam groep" autocapitalize="off">
   </label>
   <button type='submit' id="ontsleutel-knop" class="ontsleutel">Verstuur</button>
 </form>
