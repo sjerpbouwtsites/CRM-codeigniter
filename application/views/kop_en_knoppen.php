228af2ffcf8d87815d5626f5f65e6f07aa63a3c4
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-10-21 06:18:13 +0200

    Revert "Riders"

--- application/views/kop_en_knoppen.php
+++ application/views/kop_en_knoppen.php
@@ -7,11 +7,11 @@
 
 	<div id='sleutelaars' class='sleutelaars'>
 		<div class='sleutelaars-inner'>
-			<h2 class='versleutelde-titel versleutelde-titel--kleiner'>Sjerps end-to-end encrypted crm <small>v4</small></h2>
+			<h2 class='versleutelde-titel versleutelde-titel--kleiner'>Sjerps end-to-end encrypted crm <small>v3.7</small></h2>
 			<h2 class='versleutelde-titel'>Ontsleutel voor toegang<br>tot <em><?=$tabel_naam?></em> </h2>
 			<div class='sleutelaars-inner-2'>
 				<label for='ontsleutel'>
-					<input id='ontsleutel' placeholder='wachtwoord' autocapitalize="off">
+					<input id='ontsleutel' type='password' placeholder='wachtwoord' autocapitalize="off">
 				</label>
 				</label>
 				<button id='ontsleutel-knop' class='ontsleutel'><span class='emoji-groot'>🐱‍💻</span> ontsleutel</button>
