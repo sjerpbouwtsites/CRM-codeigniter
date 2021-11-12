57467f132f54e75225c505d76bbd529c9c708928
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-11-12 03:58:11 +0100

    dubbele slashes resources

--- application/views/head/head.php
+++ application/views/head/head.php
@@ -12,45 +12,45 @@
 		<link rel='stylesheet' href="<?= base_url('css/nav.css') ?>">
 		<meta http-equiv="Cache-control" content="no-cache">
 		<meta name="viewport" content="width=device-width">
-		<link rel="shortcut icon" href="<?= base_url() ?>/afb/app-icons/favicon.ico" />
-		<link rel="apple-touch-icon" sizes="57x57" href="<?= base_url() ?>/afb/app-icons/little-penguin-57-27563.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="<?= base_url() ?>/afb/app-icons/little-penguin-72-27563.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="<?= base_url() ?>/afb/app-icons/little-penguin-114-27563.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="<?= base_url() ?>/afb/app-icons/little-penguin-120-27563.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="<?= base_url() ?>/afb/app-icons/little-penguin-144-27563.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="<?= base_url() ?>/afb/app-icons/little-penguin-152-27563.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url() ?>/afb/app-icons/little-penguin-32-27563.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>/afb/app-icons/little-penguin-16-27563.png">
+		<link rel="shortcut icon" href="<?= base_url('afb/app-icons/favicon.ico') ?>" />
+		<link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('afb/app-icons/little-penguin-57-27563.png') ?>">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('afb/app-icons/little-penguin-72-27563.png') ?>">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('afb/app-icons/little-penguin-114-27563.png') ?>">
+		<link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('afb/app-icons/little-penguin-120-27563.png') ?>">
+		<link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('afb/app-icons/little-penguin-144-27563.png') ?>">
+		<link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('afb/app-icons/little-penguin-152-27563.png') ?>">
+		<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('afb/app-icons/little-penguin-32-27563.png') ?>">
+		<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('afb/app-icons/little-penguin-16-27563.png') ?>">
 		<!--		<link rel="manifest" href="/manifest.json">-->
 		<meta name="msapplication-TileColor" content="#d73f3f">
-		<meta name="msapplication-TileImage" content="<?= base_url() ?>/afb/app-icons/little-penguin-144-27563.png">
+		<meta name="msapplication-TileImage" content="<?= base_url('afb/app-icons/little-penguin-144-27563.png') ?>">
 		<meta name="theme-color" content="#d73f3f">
 		<base href='<?= base_url() ?>'>
 		<style>
 			body.lenin-poster {
-				background-image: url("<?= base_url() ?>/volg-het-ware-pad.jpg");
+				background-image: url("<?= base_url('volg-het-ware-pad.jpg') ?>");
 			}
 
 			.formulier-en-controls.categorie-leden {
-				background-image: url('<?= base_url() ?>/afb/leden-categorie-achtergrond.png');
+				background-image: url('<?= base_url("afb/leden-categorie-achtergrond.png") ?>');
 			}
 
 			.formulier-en-controls.categorie-contacten {
-				background-image: url('<?= base_url() ?>/afb/contacten-categorie-achtergrond.png');
+				background-image: url('<?= base_url("afb/contacten-categorie-achtergrond.png") ?>');
 			}
 
 			.formulier-en-controls.categorie-bondgenoten {
-				background-image: url('<?= base_url() ?>/afb/bondgenoten-categorie-achtergrond.png');
+				background-image: url('<?= base_url("afb/bondgenoten-categorie-achtergrond.png") ?>');
 			}
 
 			.start-bewerken-cel {
 
-				background-image: url("<?= base_url() ?>/img/account-edit.png");
+				background-image: url("<?= base_url('img/account-edit.png') ?>");
 
 			}
 
 			.rij-in-handmatige-selectie {
-				background-image: url("<?= base_url() ?>/img/select-group.png");
+				background-image: url("<?= base_url('img/select-group.png') ?>");
 			}
 		</style>
 	</head>
